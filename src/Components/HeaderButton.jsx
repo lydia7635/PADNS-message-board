11486cb5ac5225b233791a3ca546a1be7a123296
lydia7635 <lydia7635@gmail.com>
2022-03-12 22:24:02 +0800

    style: cursor pointer with header buttons

--- src/Components/HeaderButton.jsx
+++ src/Components/HeaderButton.jsx
@@ -12,7 +12,7 @@ const HeaderButton = ({ name, selected }) => {
           selected
             ? "bg-indigo-700 text-white"
             : "hover:bg-indigo-800 text-indigo-100 hover:text-white",
-          "px-3 py-2 rounded-md text-lg"
+          "cursor-pointer px-3 py-2 rounded-md text-lg"
         )}
       >
         {name}
